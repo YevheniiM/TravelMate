ee56ab9d31233a1481925936392e73aa8cff1877
yevhenii <xmolodtsov@gmail.com>
2019-07-03 21:22:11 +0300

    Bot response strings added.

---
+++ helpers/bot_responses.py
@@ -0,0 +1,11 @@
+GREETING_RESPONSE_1 = "Great! We’ve made an Insta Tour for you this time! Do you know how it works?"
+GREETING_RESPONSE_2 = "Alright, let me explain you in a few words how our tours work. " \
+                      "Basically, it’s a quest. We’ve found and put together locations " \
+                      "that are both instagram-worthy and interesting, also places to " \
+                      "have some food, drink and fun! You will be having tasks on where " \
+                      "to go and what to do. After you accomplish one, you will be told " \
+                      "about next one, and so on. " \
+                      "Wanna start?"
+GREETING_RESPONSE_3 = "Sad to hear that, {}. But that’s not a problem! When would you like to carry on with this trip?"
+
+HARD_TO_RECOGNIZE = "Sorry, I didn't understand. Can you ask me again? :)"
