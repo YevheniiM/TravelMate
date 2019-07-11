879862ddb02f198c34090d22f9d8174f3f022705
yevhenii <xmolodtsov@gmail.com>
2019-07-12 00:10:16 +0300

    Environment variables added

---
+++ helpers/secret_constants.py
@@ -0,0 +1,12 @@
+"""
+constants.py
+
+The module has all the needed global constants.
+
+"""
+import os
+
+PROJECT_ID = os.environ['PROJECT_ID']
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
+KEY_FILE = os.environ['KEY_FILE']
